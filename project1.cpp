b8b85b3608b2544c3f035803b16c664aa092dd7a
Adele Kelley <54007195+eleda77@users.noreply.github.com>
2025-09-30 17:11:31 -0400

    fixed phase 2 again

--- project1.cpp
+++ project1.cpp
@@ -127,13 +127,9 @@ int main(int argc, char* argv[]) {
      */
     // Take the values from the memory vector and convert to 32-bit binary
     // write to file
-    for (const auto& memory_line : memory) {
-        std::istringstream iss(memory_line);
-        std::string token;
-        while (iss >> token) {
-            int value = std::stoi(token);
-            write_binary(value, static_outfile);
-        }
+    for (const auto& token : memory) {
+        int value = std::stoi(token);
+        write_binary(value, static_outfile);
     }
 
 
