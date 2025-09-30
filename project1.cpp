df7c9732884a000b002e757dbcc24a5f62c04041
luerniam <lena.kim@richmond.edu>
2025-09-29 23:39:32 -0400

    update i-type instructions

--- project1.cpp
+++ project1.cpp
@@ -125,61 +125,79 @@ int main(int argc, char* argv[]) {
     for(std::string inst : instructions) {
         std::vector<std::string> terms = split(inst, WHITESPACE+",()");
         std::string inst_type = terms[0];
+        // R-type
         if (inst_type == "add") {
             int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 32);
-            write_binary(encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 32),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "sub") {
             int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 34);
-            write_binary(encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 34),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "mult") {
             int result = encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 24);
-            write_binary(encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 24),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "div") {
             int result = encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 26);
-            write_binary(encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 26),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "mflo") {
             int result = encode_Rtype(0, 0, 0, registers[terms[1]], 0, 18);
-            write_binary(encode_Rtype(0, 0, 0, registers[terms[1]], 0, 18),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "mfhi") {
             int result = encode_Rtype(0, 0, 0, registers[terms[1]], 0, 16);
-            write_binary(encode_Rtype(0, 0, 0, registers[terms[1]], 0, 16),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "sll") {
-            int result = encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], registers[terms[3]], 0);
-            write_binary(encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], registers[terms[3]], 0),inst_outfile);
+            int result = encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], std::stoi(terms[3]), 0);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "srl") {
-            int result = encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], registers[terms[3]], 2);
-            write_binary(encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], registers[terms[3]], 2),inst_outfile);
+            int result = encode_Rtype(0, 0, registers[terms[2]], registers[terms[1]], std::stoi(terms[3]), 2);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "slt") {
-            int result = encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], registers[terms[3]], 42);
-            write_binary(encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], registers[terms[3]], 42),inst_outfile);
+            int result = encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 42);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "jr") {
             int result = encode_Rtype(0, registers[terms[1]], 0, 0, 0, 8);
-            write_binary(encode_Rtype(0, registers[terms[1]], 0, 0, 0, 8),inst_outfile);
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "jalr") {
-            int result = encode_Rtype(0, registers[terms[1]], 0, registers[terms[2]], 0, 9);
-            write_binary(encode_Rtype(0, registers[terms[1]], 0, registers[terms[2]], 0, 9),inst_outfile);
+            if (terms.size() == 2) {
+                int result = encode_Rtype(0, registers[terms[1]], 0, 31, 0, 9);
+                write_binary(result, inst_outfile);
+            } else {
+                int result = encode_Rtype(0, registers[terms[1]], 0, registers[terms[2]], 0, 9);
+                write_binary(result, inst_outfile);
+            }
         }
         else if (inst_type == "syscall") {
-            // 
+            int result = encode_Rtype(0, 0, 0, 26, 0, 12);
+            write_binary(result, inst_outfile);
         }
+        // I-type
         else if (inst_type == "addi") {
-            int result = encode_Itype(8, registers[terms[2]], registers[terms[1]], stoi(terms[3]));
-            write_binary(encode_Itype(8, registers[terms[2]], registers[terms[1]], stoi(terms[3])),inst_outfile);
+            int result = encode_Itype(8, registers[terms[2]], registers[terms[1]], std::stoi(terms[3]));
+            write_binary(result, inst_outfile);
         }
         else if (inst_type == "beq") {
-            int result = encode_Itype(4, registers[terms[1]], registers[terms[2]], stoi(terms[3]));
-            write_binary(encode_Itype(4, registers[terms[1]], registers[terms[2]], stoi(terms[3])),inst_outfile);
-        }  
+            int offset = main_label[terms[3]] - (main_label[terms[0]] + 1); // or pc? pc = main_label[terms[0]]
+            int result = encode_Itype(4, registers[terms[1]], registers[terms[2]], offset);
+            write_binary(result, inst_outfile);
+        }
+        else if (inst_type == "bne") {
+            int offset = main_label[terms[3]] - (main_label[terms[0]] + 1); 
+            int result = encode_Itype(5, registers[terms[1]], registers[terms[2]], offset);
+            write_binary(result, inst_outfile);
+        }
+        else if (inst_type == "lw") {
+            //
+        }
+
     }
 }
 
