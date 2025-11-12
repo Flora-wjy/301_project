1390dba2c23c105871a3ab2df356c31508bc06d2
Flora <flora.yi@richmond.edu>
2025-11-12 18:51:51 -0500

    led testcase

--- checkpoint4/Testcases/led.asm
+++ checkpoint4/Testcases/led.asm
@@ -1,3 +1,15 @@
 addi $sp, $0, -4096         # initialization
 
-sw ___, -160(___)
+addi $t0, $0, 1
+addi $t1, $0, 0
+addi $t2, $0, 6
+
+loop:
+    sw $t0, -160($0)
+    sll $t0, $t0, 5
+    addi $t1, $t1, 1
+    beq $t1, $t2, endloop
+    j loop
+
+endloop:
+    j endloop
