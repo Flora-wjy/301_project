3048dfc4a2324c02f1516aa7300abd0e0769ec65
luerniam <lena.kim@richmond.edu>
2025-11-12 18:48:34 -0500

    buzzer testcase

--- checkpoint4/Testcases/buzzer.asm
+++ checkpoint4/Testcases/buzzer.asm
@@ -1,5 +1,12 @@
 addi $sp, $0, -4096         # initialization
 
-sw $t0, -128($0)
-sw $t1, -124($0)
-sw $t2, -120($0)
+addi $t0, $0, 300
+addi $t1, $0, 80
+addi $t2, $0, 1
+
+loop:
+    sw $t0, -128($0)
+    sw $t1, -124($0) 
+    sw $t2, -120($0) 
+    j loop
+
