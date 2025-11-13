cb85dbe5eaad0d4c2a1db8056d259d307a8c525d
Flora <flora.yi@richmond.edu>
2025-11-12 19:38:02 -0500

    memory testcases

--- checkpoint4/Testcases/keyboard.asm
+++
@@ -1,6 +0,0 @@
-addi $sp, $0, -4096         # initialization
-
-lw $t0, -240($0)
-lw $t1, -236($0)
-
-sw $t2, -240($0)
--- checkpoint4/Testcases/memory.asm
+++ checkpoint4/Testcases/memory.asm
@@ -1,7 +1,22 @@
-addi $sp, $0, -4096         # initialization
+.data
+    info: .word 104 101 108 108 111 44 32 119 111 114 108 100 33 10 0
+main:
+    addi $sp, $0, -4096         # initialization
+    addi $t2, $0, 50
+    la $t1, info
 
-sw ___, -252(___)           # normal sw
-lw ___, -252(___)           # normal lw
+loop:
+    lw $t0, 0($t1)
 
-sw ___, -252(___)           # do nothing
-lw ___, -252(___)           # do nothing
+    sw $t0, -260($0)           # normal sw
+    slt $t3, $t0, $t2           # t0 < 50
+    bne $t3, $0, special
+    addi $t0, $t0, -32
+    special:
+    sw $t0, -256($0)
+    lw $t0, -260($0)           # normal lw
+    sw $t0, -256($0)
+    addi $t1, $t1, 4
+
+    j loop
+
